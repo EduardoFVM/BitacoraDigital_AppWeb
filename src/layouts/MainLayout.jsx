454d37c958d63ddf480e1a27eb70ea09edf58052
ElChavoDelBronx <20243ds087@utez.edu.mx>
2026-03-01 13:28:45 -0600

    Scroll agregado a todas las vistas

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -6,18 +6,18 @@ export default function MainLayout(){
     const SIDEBAR_WIDTH = "80px";
 
     return(
-        <div className="d-flex">
+        <div className="d-flex vh-100 overflow-hidden">
             <CustomSidebar />
             <div
                 className="d-flex flex-column w-100"
                 style={{
                     marginLeft: SIDEBAR_WIDTH,
-                    minHeight: "100vh",
+                    Height: "100vh",
                     transition: "margin-left 0.3s ease"
                 }}
             >
                 <CustomNavbar />
-                <div className="p-4">
+                <div className="p-4 flex-grow-1 overflow-y-auto">
                     <Outlet/>
                 </div>
             </div>
--- src/modules/auth/Reportes.jsx
+++ src/modules/auth/Reportes.jsx
@@ -40,39 +40,91 @@ export default function Reportes(){
                 </div>
                 <div className="col-12">
                     <div className="border rounded-3 overflow-hidden">
-                    
-                    <table className="table mb-0 align-middle">
-                        
-                        <thead>
-                        <tr>
-                            <th className="py-3"><h3>Reporte General Por Proyecto</h3></th>
-                        </tr>
-                        <tr>
-                            <th className="fw-medium px-3 py-3">PROYECTO</th>
-                            <th className="fw-medium px-3 py-3">ESTUDIANTES</th>
-                            <th className="fw-medium px-3 py-3">TAREAS</th>
-                            <th className="fw-medium px-3 py-3">HORAS</th>
-                            <th className="fw-medium px-3 py-3">AVANCE</th>
-                        </tr>
-                        </thead>
-                        
-                        <tbody>
-                        <tr>
-                            <td className="px-3 py-3">
-                            <div className="fw-semibold">Sistema de inventarios</div>
-                            </td>
-                            <td className="text-body-secondary px-3">8</td>
-                            <td className="text-body-secondary px-3">3</td>
-                            <td>
-                            <span className="badge rounded-pill bg-success bg-opacity-10 text-success px-3 py-2">
-                                Activo
-                            </span>
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
+                        <table className="table mb-0 align-middle">
+                            <thead>
+                            <tr>
+                                <th className="py-3"><h3>Reporte General Por Proyecto</h3></th>
+                            </tr>
+                            <tr>
+                                <th className="fw-medium px-3 py-3">PROYECTO</th>
+                                <th className="fw-medium px-3 py-3">ESTUDIANTES</th>
+                                <th className="fw-medium px-3 py-3">TAREAS</th>
+                                <th className="fw-medium px-3 py-3">HORAS</th>
+                                <th className="fw-medium px-3 py-3">AVANCE</th>
+                            </tr>
+                            </thead>
+                            
+                            <tbody>
+                            <tr>
+                                <td className="px-3 py-3">
+                                <p className="fw-semibold">Sistema de inventarios</p>
+                                </td>
+                                <td className="text-body-secondary px-3">8</td>
+                                <td className="text-body-secondary px-3">3</td>
+                                <td>
+                                <span className="badge rounded-pill bg-success bg-opacity-10 text-success px-3 py-2">
+                                    Activo
+                                </span>
+                                </td>
+                                <td className="px-3">
+                                    <div className="d-flex align-items-center">
+                                        <div className="progress w-100 me-2" style={{ height: '13px' }}>
+                                            <div 
+                                                className="progress-bar" 
+                                                style={{ width: `10%` }} 
+                                            />
+                                        </div>
+                                        <p className="mb-0">10%</p>
+                                    </div>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
+                <div className="row mt-5 d-flex justify-content-between">
+                                    <div className="col-6 mb-5" style={{width: '990px'}}>
+                                        <BaseCard>
+                                            <div className="d-flex px-4 pt-5 row">   
+                                                <div className="col-12 mb-2 d-flex justify-content-between">
+                                                    <p className="h4 fw-bold">[Titulo]</p>
+                                                    
+                                                </div>
+                                                <div className="col-12 mb-2">
+                                                    <p className="h5">[Texto]</p>
+                                                </div>
+                                                <div className="mb-5">
+                                                    <div>
+                                                        <div className="progress" style={{ height: '13px' }}>
+                                                            <div 
+                                                            className="progress-bar" 
+                                                            style={{ width: `10%` }} 
+                                                            />
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className="col-12 gap-3 mb-3 d-flex">
+                                                    <div className="container text-center rounded-4 p-3" style={{backgroundColor: '#f9fafb'}}>
+                                                        <p className="h2 fw-bold">18/24</p>
+                                                        <p>Tareas</p>
+                                                    </div>
+                                                    <div className="container text-center rounded-4 p-3" style={{backgroundColor: '#f9fafb'}}>
+                                                        <p>[Numero_Horas]</p>
+                                                        <p>Horas totales</p>
+                                                    </div>
+                                                    <div className="container text-center rounded-4 p-3" style={{backgroundColor: '#f9fafb'}}>
+                                                        <p>[Numero_Estudiantes]</p>
+                                                        <p>Estudiantes</p>
+                                                    </div>
+                                                </div>
+                                                <div className="col-12 text-end">
+                                                    <p>[Estado]</p>
+                                                </div>
+                                            </div>
+                                        </BaseCard>
+                                    </div>
+                                    
+                                </div>
             </div>
         </div>
     );
